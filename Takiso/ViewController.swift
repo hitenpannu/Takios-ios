abf1337fd479b55c148ab32bc362a00140765ce7
hpannu <hitender.pannu@agoda.com>
2020-06-07 14:24:31 +0700

    Initial Commit

---
+++ Takiso/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Takiso
+//
+//  Created by Pannu, Hitender (Agoda) on 7/6/20.
+//  Copyright © 2020 Pannu, Hitender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
